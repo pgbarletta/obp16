86dd39fe2865b6bfbec95ffbcc932a1feb6d928a
pgbarletta <pbarletta@gmail.com>
2017-04-17 17:30:54 -0300

    Redone 1st analysis

---
+++ bu/toroit/largar.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+./wt_apo/run/1_wt_apo_run.sh
+./wt_eol/run/1_wt_eol_run.sh
+./wt_ctv/run/1_wt_ctv_run.sh
+
+./mu_apo/run/1_mu_apo_run.sh
+./mu_eol/run/1_mu_eol_run.sh
+./mu_ctv/run/1_mu_ctv_run.sh
